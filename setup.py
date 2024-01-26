0f695ffd4b4af7921bfc4d3dde120c0f27e62a0d
Nguyen Duy Minh Thong <minhthong.nguyen@inkr.com>
2024-01-26 16:36:33 +0700

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="src",
+    version="1.0.0",
+    description="SQLAI Core",
+    url="http://github.com/nguyenduyminhthong/sqlai-core",
+    author="Nguyen Duy Minh Thong",
+    author_email="nguyenduyminhthong@gmail.com",
+    license="MIT",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[line.strip() for line in open("requirements.txt").readlines()],
+)
